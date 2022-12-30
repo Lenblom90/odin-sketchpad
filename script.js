efe5bb95224d6b2d8951bfcaf3ffd4604553e22c
lblom <lenblom@gmail.com>
2022-12-30 16:32:59 +0200

    Make squares responsive to window size
    Square size is now dependent on the initial container size and the total
    squares.

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 const container = document.querySelector(".container");
-let squares = 16;
 
-for(i=0; i<squares*squares; i++){
-    let div = document.createElement("div");
-    div.classList.add("gridDiv");
-    div.addEventListener('mouseenter', () => div.style.backgroundColor="#ffffff");
-    container.appendChild(div);
+function setSquares(){  
+    while (container.lastChild){
+        container.removeChild(container.lastChild);
+    }
+    let userSquares = prompt("How many squares do you want?");
+    userSquares = userSquares < 101 ? userSquares : 100;
+    drawSquares(userSquares);
 }
+
+function drawSquares(squares){
+    for(i=0; i<squares; i++)
+    {
+    let row = document.createElement("div");
+    for(j=0; j<squares; j++){
+        let div = document.createElement("div");
+        div.style.width = `${window.innerWidth / squares}px`;
+        div.style.height = `${window.innerHeight / squares}px`;     
+        div.classList.add("gridDiv");
+        div.addEventListener('mouseenter', () => div.style.backgroundColor="#ffffff");
+        row.appendChild(div);
+    }
+    row.classList.add('row')
+    container.appendChild(row);
+    };
+}
+
+drawSquares(10);
